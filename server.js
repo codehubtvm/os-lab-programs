a1ad803b9e4d84d18e9a09032db9b87467ef63df
codehubtvm <anandhukrishnastvm@gmail.com>
2026-04-03 15:36:53 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const fs = require("fs");
+const { exec } = require("child_process");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/run", (req, res) => {
+    const code = req.body.code;
+
+    fs.writeFileSync("program.c", code);
+
+    exec("gcc program.c -o program && ./program", (err, stdout, stderr) => {
+        if (err) {
+            return res.json({ output: stderr });
+        }
+        res.json({ output: stdout });
+    });
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
