f5230cb9560dbab4de4437a65f7dc3c042efd66a
codehubtvm <anandhukrishnastvm@gmail.com>
2026-04-03 10:49:51 +0000

    port fix

--- server.js
+++ server.js
@@ -36,6 +36,10 @@ app.get("/", (req, res) => {
     res.send("Server is working ✅");
 });
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log("🔥 Server running on port " + PORT);
+});
     console.log("🔥 Server running on port 5000");
 });
